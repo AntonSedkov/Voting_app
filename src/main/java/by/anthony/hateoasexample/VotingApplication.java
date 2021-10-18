2fce0368a5f1df06aaad8fbfa47059639ad10769
User from script <S3dk0vAG@gmail.com>
2021-10-18 21:17:19 +0300

    init commit

---
+++ src/main/java/by/anthony/hateoasexample/VotingApplication.java
@@ -0,0 +1,13 @@
+package by.anthony.hateoasexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/anthony/hateoasexample/VotingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.anthony.hateoasexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
