95afe2a0b312425177a16c4dd0f0771f88fe3258
User from script <S3dk0vAG@gmail.com>
2021-10-19 22:00:22 +0300

    some refactoring

--- src/main/java/by/anthony/hateoasexample/VotingApplication.java
+++ src/main/java/by/anthony/hateoasexample/VotingApplication.java
@@ -1,23 +1,12 @@
 package by.anthony.hateoasexample;
 
-import by.anthony.hateoasexample.repository.UserRepository;
-import lombok.AllArgsConstructor;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@AllArgsConstructor
-public class VotingApplication implements ApplicationRunner {
-    private final UserRepository userRepository;
+public class VotingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VotingApplication.class, args);
-    }
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        System.out.println(userRepository.findByLastNameContainingIgnoreCase("e"));
     }
 }
